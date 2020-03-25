f126b7be1a70f0e7cbf9105c5bb71945cf4182a3
guilherme santanna <guilhermesantanna.mail@gmail.com>
2020-03-25 15:04:53 -0300

    codigo finalizado

--- server.js
+++ server.js
@@ -10,58 +10,45 @@ const server =express()
 
 
 
-
-
-
-
-
-
-// configurar servidor para apresentar arquivos static
 server.use(express.static('public'))
+
+
 
 server.use(express.urlencoded({extended:true}))
 
+
+
+const Pool =require('pg').Pool
+const db = new Pool({
+    user:'postgres',
+    password:'1310',
+    host:'localhost',
+    port:5432,
+    database:'doadores'
+})
+
+
+
 // configuração do nunjecks
-const nunjucks =require("nunjucks")
-nunjucks.configure("./",{
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
     express: server,
-    noCache:true,
+    noCache: true,
 
 })
 
-const doadores = [
-    {
-        nome: "Guilherme peireira",
-        sangue: "AB+"
-
-
-    },
-    {
-        nome: "guilherme santanna",
-        sangue: "AB+"
-
-
-    },
-    {
-        nome: "guilherme santanna",
-        sangue: "AB+"
-
-
-    },
-    {
-        nome: "guilherme santanna",
-        sangue: "AB+"
-
-
-    },
-
-
-]
-
-// uso de get 
+//uso do get
 server.get("/",function(req, res){
-    return res.render("index.html",{doadores})
+    
+    db.query("SELECT * FROM doadores", function(err, result){
+        if(err) return rest.send("erro bando de dados.")
+
+        const doadores = result.rows
+        return res.render("index.html",{doadores})
+    })
+
 })
+
 
 //uso do post
 server.post("/", function (req,res){
@@ -69,18 +56,29 @@ server.post("/", function (req,res){
     const email= req.body.email
     const sangue= req.body.sangue
 
+    if (nome =="" || email=="" || sangue==""){
+        return res.send("Todos os cambos são Obrigatorios")
+    }
 
-//adc pra dentro do arrey
-    doadores.push({
-        nome:nome,
-        sangue,sangue
-    })
-    console.log(doadores)
-    return  res.redirect("/")
 
-    
+    const query =
+        `insert into "doadores" ("nome","email","sangue")
+             VALUES ($1, $2, $3)` 
 
+
+		db.query(query[nome,email,sangue])
+
+             const VALUES=[nome,email,sangue]
+
+             db.query(query, VALUES, function(err){
+                 if(err) return res.send("erro bando de dados.",VALUES)
+
+                 
+                 return res.redirect('/')
+             } )
+          
 })
+
 
 server.listen(3000)
 
